3828d64e48b244619932f5c367f499e5b79901d2
Lucas Wiener <lucas@wiener.se>
2014-04-30 10:57:03 +0200

    Now being able to store verifications with accounts. Basic debit credit sums of accounts/verifications with date filter. Tests in place

--- bookie.js
+++ bookie.js
@@ -16,6 +16,286 @@
             return bookie;
         }
 
+        //==================== Account ====================
+
+        var Account = (function() {
+            function Account(book, number, name) {
+                if(!isValidBook(book)) {
+                    throw new Error("Account must have a valid Book instance reference.");
+                }
+
+                if(!_.isNumber(number) || !_.isString(name)) {
+                    throw new Error("An account must have a number and a name.");
+                }
+
+                this.book = book;
+                this.number = number;
+                this.name = name;
+
+                this.debits = [];
+                this.credits = [];
+            }
+
+            Account.prototype.sumDebit = function(from, to) {
+                return sumTransactions(this.debits, from, to);
+            };
+
+            Account.prototype.sumCredit = function(from, to) {
+                return sumTransactions(this.credits, from, to);
+            };
+
+            function sumTransactions(container, from, to) {
+                debugger;
+                from = parseDate(from);
+                to = parseDate(to);
+
+                function isVerificationInsideDates(v) {
+                    return (!from || v.date.getTime() >= from.getTime()) && (!to || v.date.getTime() <= to.getTime());
+                }
+
+                return _.reduce(container, function(sum, transcation) {
+                    return sum + (isVerificationInsideDates(transcation.verification) ? transcation.amount : 0);
+                }, 0) || 0;
+            }
+
+            return Account;
+        })();
+
+        //==================== Verification ====================
+
+        var Verification = (function() {
+            function Verification(book, date, text) {
+                if(!isValidBook(book)) {
+                    throw new Error("Verification must have a valid Book instance reference.");
+                }
+
+                if(!_.isDate(date) || !_.isString(text)) {
+                    throw new Error("A verification must have a date and a text.");
+                }
+
+                this.book = book;
+                this.date = date;
+                this.text = text;
+                this.number = book.getNextVerificationNumber();
+
+                this.debits = [];
+                this.credits = [];
+            }
+
+            function accountTransaction(verification, type, account, amount) {
+                if(!isValidVerification(verification)) {
+                    throw new Error("Invalid verification.");
+                }
+
+                var book = verification.book;
+
+                if(!isValidAccount(book, account)) {
+                    var accountnr = account;
+                    account = book.getAccount(account);
+
+                    if(!account) {
+                        throw new Error("Invalid account: " + accountnr);
+                    }
+                }
+
+                amount = bookie.round(amount);
+
+                if(amount <= 0) {
+                    throw new Error("Amount cannot be less or equal to zero.");
+                }
+
+                var vContainer = verification[type + "s"];
+                var aContainer = account[type + "s"];
+
+                vContainer.push({
+                    account: account,
+                    amount: amount
+                });
+
+                aContainer.push({
+                    verification: verification,
+                    amount: amount
+                });
+            }
+
+            /**
+             * Debits an account in the verification.
+             * @param {Account | Number} account The account to debit. If number, the account will be fetched from the book.
+             */
+            Verification.prototype.debit = function(account, amount) {
+                accountTransaction(this, "debit", account, amount);
+                return this;
+            }
+
+            /**
+             * Credits an account in the verification.
+             * @param {Account | Number} account The account to credit. If number, the account will be fetched from the book.
+             */
+            Verification.prototype.credit = function(account, amount) {
+                accountTransaction(this, "credit", account, amount);
+                return this;
+            }
+
+            /**
+             * Checks so that the sum of credit amounts equal the sum of debit amounts.
+             * @return {bool} True if the verification is balanced. False otherwise.
+             */
+            Verification.prototype.isBalancedCreditDebit = function() {
+                return sumTransactions(this.debits) === sumTransactions(this.credits);
+            }
+
+            function sumTransactions(container) {
+                return _.reduce(container, function(sum, transaction) {
+                    return sum + transaction.amount;
+                }, 0);
+            }
+
+            return Verification;
+        })();
+        
+
+        //==================== Book ====================
+
+        var Book = (function() {
+            /**
+             * Creates a bookkeeping book that holds verifications on account changes.
+             */
+            function Book() {
+                //The accounts of the book are stored as the account number as key.
+                this.accounts = {};
+
+                //The array of verifications. Will be kept ordered by verification id.
+                this.verifications = [];
+
+                this.nextVerificationNumber = 1;
+            }
+
+            /**
+             * Creates a new account and adds it to the book.
+             */
+            Book.prototype.createAccount = function(number, name) {
+                if(this.accounts[number]) {
+                    throw new Error("An account with number " + number + "already exists.");
+                }
+
+                var account = new Account(this, number, name)
+                this.accounts[number] = account;
+
+                return account;
+            }
+
+            Book.prototype.getAccount = function(number) {
+                if(!_.isNumber(number)) {
+                    throw new Error("Invalid account number.");
+                }
+
+                var account = this.accounts[number];
+
+                return account || null;
+            }
+
+            Book.prototype.createVerification = function(date, text) {
+                var verification = new Verification(this, parseDate(date), text);
+
+                this.verifications.push(verification);
+
+                return verification;
+            }
+
+            Book.prototype.getNextVerificationNumber = function() {
+                return this.nextVerificationNumber++;
+            }
+
+            return Book;
+        })();
+
+        //==================== Helpers ====================
+
+        function parseDate(date) {
+            function zeroFix(index) {
+              if(parts[index].length == 1) {
+                parts[index] = '0' + parts[index];
+              }
+            }
+
+            if(_.isDate(date)) {
+                return date;
+            }
+
+            if(!date || !_.isString(date)) {
+                return null;
+            }
+
+            var parts = date.split('-');
+
+            if(parts.length !== 3) {
+                return null;
+            }
+
+            zeroFix(1);
+            zeroFix(2);
+
+            return new Date(parts.join('-'));
+        }
+
+        function round(number, decimals) {
+            function _round(number, exp) {
+                exp = +exp;
+                number = +number;
+
+                if(!number) {
+                    throw new Error("Invalid number.");
+                }
+
+                if(!exp) {
+                    return Math.round(number);
+                }
+
+                if(exp % 1 !== 0) {
+                    throw new Error("Decimals must be an integer.");
+                }
+
+                // Shift
+                number = number.toString().split('e');
+                number = Math.round(+(number[0] + 'e' + (number[1] ? (+number[1] - exp) : -exp)));
+
+                // Shift back
+                number = number.toString().split('e');
+                return +(number[0] + 'e' + (number[1] ? (+number[1] + exp) : exp));
+            }
+
+            var d = decimals ? decimals : bookie.roundDecimals;
+
+            if(!d) {
+                throw new Error("Invalid decimals.");
+            }
+
+            return _round(number, -d);
+        }
+
+        function isValidVerification(verification) {
+            return verification instanceof Verification && isValidBook(verification.book);
+        }
+
+        function isValidBook(book) {
+            return book instanceof Book;
+        }
+
+        function isValidAccount(book, account) {
+            if(!isValidBook(book)) {
+                throw new Error("Invalid book.");
+            }
+
+            return account instanceof Account && account.book === book;
+        }
+
+        //==================== Expose ====================
+
+        bookie.Book = Book;
+        bookie.round = round;
+        bookie.parseDate = parseDate;
+        bookie.roundDecimals = 2;
+
         return bookie;
     }
 
--- test/bookie_test.js
+++ test/bookie_test.js
@@ -1,15 +1,298 @@
 var bookie = require("../bookie.js");
 var chai = require("chai");
+var _ = require("lodash");
 var expect = chai.expect;
 
-describe("bookie.js", function() {
-	it("should be defined", function() {
-		expect(bookie).to.be.a("object");
-	});
+function makeTransactions(book) {
+    book.createVerification("2012-02-11", "Domain names").credit(2010, 188).debit(2640, 37.6).debit(6500, 150.4);
+    book.createVerification("2012-03-04", "Paper holders").credit(2010, 29).debit(2640, 5.8).debit(6100, 23.2);
+    book.createVerification("2012-03-09", "Office stuff").credit(2010, 31).debit(2640, 6.2).debit(6100, 24.8);
+    book.createVerification("2012-03-09", "Post stamps").credit(2010, 18).debit(2640, 3.6).debit(6100, 14.4);
+    book.createVerification("2012-03-24", "iPad").credit(2020, 7195).debit(2640, 1439).debit(5400, 5756);
+}
 
-	describe("runInContext", function() {
-		it("should be defined in bookie object", function() {
-			expect(bookie.runInContext).to.be.a("function");
-		});
-	});
+function makeAccounts(book) {
+    book.createAccount(1930, "Bank");
+    book.createAccount(2010, "Own capital John Doe");
+    book.createAccount(2020, "Own capital Jane Doe")
+    book.createAccount(2640, "Incoming VAT 25 %");
+    book.createAccount(5400, "Usage inventory");
+    book.createAccount(6100, "Office material");
+    book.createAccount(6500, "External services");
+}
+
+describe("bookie.js", function() {
+    it("should be defined", function() {
+        expect(bookie).to.be.a("object");
+    });
+
+    describe("runInContext", function() {
+        it("should be defined in bookie object", function() {
+            expect(bookie.runInContext).to.be.a("function");
+        });
+    });
+
+    describe("round", function() {
+        it("should be defined and have roundDecimals in place", function() {
+            expect(bookie.round).to.be.a("function");
+            expect(bookie.roundDecimals).to.be.a("number");
+        });
+
+        it("should round numbers to the set decimals of roundDecimals", function() {
+            bookie.roundDecimals = 2;
+            expect(bookie.round(19.3141)).to.equal(19.31);
+            expect(bookie.round(15.5)).to.equal(15.5);
+            expect(bookie.round(15.555)).to.equal(15.56);
+            expect(bookie.round(0.001)).to.equal(0);
+
+            bookie.roundDecimals = 1;
+            expect(bookie.round(19.3141)).to.equal(19.3);
+            expect(bookie.round(15.5)).to.equal(15.5);
+            expect(bookie.round(15.555)).to.equal(15.6);
+            expect(bookie.round(0.001)).to.equal(0);
+
+            bookie.roundDecimals = 2;
+        });
+
+        it("should override roundDecimals if decimals parameter is set", function() {
+            bookie.roundDecimals = 3;
+            expect(bookie.round(19.3141, 2)).to.equal(19.31);
+            expect(bookie.round(15.5, 2)).to.equal(15.5);
+            expect(bookie.round(15.555, 2)).to.equal(15.56);
+            expect(bookie.round(0.001, 2)).to.equal(0);
+
+            expect(bookie.round(19.3141, 1)).to.equal(19.3);
+            expect(bookie.round(15.5, 1)).to.equal(15.5);
+            expect(bookie.round(15.555, 1)).to.equal(15.6);
+            expect(bookie.round(0.001, 1)).to.equal(0);
+
+            bookie.roundDecimals = 2;
+        });
+
+        it("should be able to round really big numbers", function() {
+            bookie.roundDecimals = 3;
+            expect(bookie.round(1031313124120102401204102412401204102.121241512)).to.equal(1031313124120102401204102412401204102.121);
+            expect(bookie.round(1031313124120102401204102412401204114124124125125151251251202.1399)).to.equal(1031313124120102401204102412401204114124124125125151251251202.14);
+
+            bookie.roundDecimals = 2;
+        });
+    });
+
+    describe("parseDate", function() {
+        it("should be defined", function() {
+            expect(bookie.parseDate).to.be.a("function");
+        });
+
+        function validDate(date, year, month, day) {
+            expect(date.getDate()).to.equal(day);
+            expect(date.getMonth()).to.equal(month-1);
+            expect(date.getFullYear()).to.equal(year);
+        }
+
+        it("should return dates if given dates", function() {
+            var date = new Date();
+
+            expect(bookie.parseDate(date)).to.equal(date);
+        });
+
+        it("should parse string dates to dates", function() {
+            var date;
+
+            validDate(bookie.parseDate("2014-01-02"), 2014, 1, 2);
+            validDate(bookie.parseDate("2014-1-2"), 2014, 1, 2);
+        });
+
+        it("should return null if unable to parse date", function() {
+            expect(bookie.parseDate()).to.equal(null);
+            expect(bookie.parseDate("adwd")).to.equal(null);
+            expect(bookie.parseDate({wat:"sup"})).to.equal(null);
+            expect(bookie.parseDate(1412)).to.equal(null);
+            expect(bookie.parseDate(1337)).to.equal(null);
+            expect(bookie.parseDate(true)).to.equal(null);
+            expect(bookie.parseDate(1.1)).to.equal(null);
+        });
+    });
+
+    describe("Book", function() {
+        it("should have constructor defined", function() {
+            expect(bookie.Book).to.be.a("function");
+            expect((new bookie.Book()) instanceof bookie.Book).to.equal(true);
+        });
+
+        describe("createAccount", function() {
+            var book;
+
+            beforeEach(function() {
+                book = new bookie.Book();
+            });
+
+            it("should be defined", function() {
+                expect(book.createAccount).to.be.a("function");
+            });
+
+            it("Should create a new Account instance", function() {
+                var account = book.createAccount(1337, "test");
+                
+                expect(account.book).to.equal(book);
+                expect(account.number).to.equal(1337);
+                expect(account.name).to.equal("test");
+            });
+
+            it("Should not be able to create mutliple accounts with same number", function() {
+                book.createAccount(1337, "test");
+
+                expect(function() {
+                    book.createAccount(1337, "daw");
+                }).to.throw(Error);
+            });
+        });
+
+        describe("getAccount", function() {
+            var book;
+
+            beforeEach(function() {
+                book = new bookie.Book();
+            });
+
+            it("should be defined", function() {
+                expect(book.getAccount).to.be.a("function");
+            });
+
+            it("Should return accounts by number", function() {
+                expect(book.createAccount(1337, "test")).to.equal(book.getAccount(1337));
+                expect(book.getAccount(9999)).to.equal(null);
+            });
+        });
+
+        describe("createVerification", function() {
+            var book;
+
+            beforeEach(function() {
+                book = new bookie.Book();
+            });
+
+            it("should be defined", function() {
+                expect(book.createVerification).to.be.a("function");
+            });
+
+            it("should create a new Verification instance", function() {
+                var date = new Date();
+                var v = book.createVerification(date, "text");
+
+                expect(v.book).to.equal(book);
+                expect(v.number).to.equal(1);
+                expect(v.date).to.equal(date);
+                expect(v.text).to.equal("text");
+
+                v = book.createVerification(date, "test");
+                expect(v.book).to.equal(book);
+                expect(v.number).to.equal(2);
+                expect(v.date).to.equal(date);
+                expect(v.text).to.equal("test");
+            });
+
+            it("should accept date strings as date", function() {
+                var v = book.createVerification("2013-01-2", "text");
+
+                expect(v.book).to.equal(book);
+                expect(v.number).to.equal(1);
+                expect(v.date.getTime()).to.equal((new Date("2013-01-02")).getTime());
+                expect(v.text).to.equal("text"); 
+            });
+        });
+
+        describe("Verifications", function() {
+            var book;
+
+            beforeEach(function() {
+                book = new bookie.Book();
+
+                makeAccounts(book);
+            });
+
+            it("should be able to transact accounts", function() {
+                var v = book.createVerification("2012-02-11", "Domain names");
+                v.credit(2010, 188);
+                v.debit(2640, 37.6);
+                v.debit(6500, 150.4);
+
+                function sum(container) {
+                    return _.reduce(container, function(sum, transaction) {
+                        return sum + transaction.amount;
+                    }, 0);
+                }
+
+                expect(sum(v.credits)).to.equal(188);
+                expect(sum(v.debits)).to.equal(188);
+
+                //Should also work to transact with account objects.
+                v = book.createVerification("2012-03-04", "Paper holders");
+                v.credit(book.getAccount(2010), 29);
+                v.debit(book.getAccount(2640), 5.8);
+                v.debit(book.getAccount(6100), 23.2);
+
+                expect(sum(v.credits)).to.equal(29);
+                expect(sum(v.debits)).to.equal(29);
+            });
+
+            it("should be able to validate credit and debit balance", function() {
+                makeTransactions(book);
+
+                book.verifications.forEach(function(v) {
+                    expect(v.isBalancedCreditDebit(), "V. nr: " + v.number).to.equal(true);
+                });
+            });
+        });
+
+        describe("Accounts", function() {
+            var book;
+
+            beforeEach(function() {
+                book = new bookie.Book();
+
+                makeAccounts(book);
+                makeTransactions(book);
+            });
+
+            it("should be able to sum transactions", function() {
+                expect(book.getAccount(2010).sumDebit()).to.equal(0)
+                expect(book.getAccount(2010).sumCredit()).to.equal(266);
+
+                expect(book.getAccount(2020).sumDebit()).to.equal(0);
+                expect(book.getAccount(2020).sumCredit()).to.equal(7195);
+
+                expect(book.getAccount(2640).sumDebit()).to.equal(1492.2);
+                expect(book.getAccount(2640).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(5400).sumDebit()).to.equal(5756);
+                expect(book.getAccount(5400).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(6100).sumDebit()).to.equal(62.4);
+                expect(book.getAccount(6100).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(6500).sumDebit()).to.equal(150.4);
+                expect(book.getAccount(6500).sumCredit()).to.equal(0);
+            });
+
+            it("should be able to sum transactions by date filter", function() {
+                expect(book.getAccount(2010).sumDebit("2012-02-11")).to.equal(0)
+                expect(book.getAccount(2010).sumCredit("2012-03-09")).to.equal(49);
+
+                expect(book.getAccount(2020).sumDebit()).to.equal(0);
+                expect(book.getAccount(2020).sumCredit(null, "2012-03-24")).to.equal(7195);
+
+                expect(book.getAccount(2640).sumDebit("2012-03-04", "2012-03-09")).to.equal(15.6);
+                expect(book.getAccount(2640).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(5400).sumDebit("2014-01-01")).to.equal(0);
+                expect(book.getAccount(5400).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(6100).sumDebit("2012-03-04", "2012-03-04")).to.equal(23.2);
+                expect(book.getAccount(6100).sumCredit()).to.equal(0);
+
+                expect(book.getAccount(6500).sumDebit("2011-1-1", "2015-1-1")).to.equal(150.4);
+                expect(book.getAccount(6500).sumCredit()).to.equal(0);
+            });
+        });
+    });
 });
