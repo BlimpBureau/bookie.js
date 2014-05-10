c8dce9f95e217683b14aef653c9795fc2c043378
Lucas Wiener <lucas@wiener.se>
2014-05-10 17:53:04 +0200

    Balance and result now working as expected

--- test/extensions/swedish-hb-ef_test.js
+++ test/extensions/swedish-hb-ef_test.js
@@ -38,16 +38,76 @@ describe("SwedishHBEF", function() {
 
             expect(book.getAccount(2010).name).to.equal("Eget kapital");
             expect(book.getAccount(2020)).to.equal(null);
+            expect(book.owners).to.eql([]);
+            expect(book.extensions.SwedishHBEF.types.ownCapital).to.equal("Eget kapital");
         });
 
         it("should respond to owners option", function() {
+            var owners = [{ name: "John" }, { name: "Jane" }];
+
             book.use(bookieSwedishHBEF({
-                owners: [{ name: "John" }, { name: "Jane" }]
+                owners: owners
             }));
 
             expect(book.getAccount(2010).name).to.equal("Eget kapital John");
             expect(book.getAccount(2020).name).to.equal("Eget kapital Jane");
             expect(book.getAccount(2030)).to.equal(null);
+
+            expect(book.owners).to.eql(owners);
+            expect(book.extensions.SwedishHBEF.types.ownCapital).to.equal("Eget kapital");
+            expect(book.extensions.SwedishHBEF.types.ownCapitalOwner1).to.equal("Eget kapital John");
+            expect(book.extensions.SwedishHBEF.types.ownCapitalOwner2).to.equal("Eget kapital Jane");
+            expect(book.classifiers[book.extensions.SwedishHBEF.types.ownCapitalOwner1]).to.be.an("array");
+            expect(book.classifiers[book.extensions.SwedishHBEF.types.ownCapitalOwner2]).to.be.an("array");
+
+            expect(book.getAccounts(book.extensions.SwedishHBEF.types.ownCapitalOwner1)).to.eql([book.getAccount(2010)]);
+            expect(book.getAccounts(book.extensions.SwedishHBEF.types.ownCapitalOwner2)).to.eql([book.getAccount(2020)]);
+        });
+    });
+
+    describe("createVerification", function() {
+        var book;
+
+        beforeEach(function() {
+            book = new bookie.Book();
+            book.use(bookieSwedishHBEF({
+                owners: [{ name: "John" }, { name: "Jane" }]
+            }));
+        });
+
+        it("should add default owners object to verifications", function() {
+            book.createVerification("2012-03-01", "testing").credit(2010, 29).debit(2640, 5.8).debit(6100, 23.2);
+
+            expect(book.getVerification(1).owners).to.eql({
+                "John": 0.5,
+                "Jane": 0.5
+            });
+        });
+
+        it("should respond to owners object parameter", function() {
+            var share = { "John": 0.1, "Jane": 0.9 };
+            book.createVerification("2012-03-01", "testing", share).credit(2010, 29).debit(2640, 5.8).debit(6100, 23.2);
+            expect(book.getVerification(1).owners).to.eql(share);
+
+            book.createVerification("2014-12-04", "something else", { "Jane": 0.34 });
+            expect(book.getVerification(2).owners).to.eql({
+                "John": 0.66,
+                "Jane": 0.34
+            });
+        });
+
+        it("should respond to owners array parameter", function() {
+            book.createVerification("2102-03-12", "test", [0.12, 0.88]);
+            expect(book.getVerification(1).owners).to.eql({
+                "John": 0.12,
+                "Jane": 0.88
+            });
+
+            book.createVerification("2012-03-01", "something", [0.9]);
+            expect(book.getVerification(2).owners).to.eql({
+                "John": 0.9,
+                "Jane": 0.1
+            });
         });
     });
 
@@ -94,8 +154,6 @@ describe("SwedishHBEF", function() {
         });
 
         it("should return a balance object", function() {
-            makeTransactions(book);
-
             var balance = book.balance();
 
             expect(balance).to.be.an("object");
@@ -103,7 +161,12 @@ describe("SwedishHBEF", function() {
             expect(balance.to).to.equal(undefined);
             expect(balance.assets).to.equal(0);
             expect(balance.debts).to.equal(0);
+            expect(balance.ownCapital).to.equal(0);
             expect(balance.valid).to.equal(true);
+            expect(balance.ownCapitalShare).to.eql({
+                "John": 0,
+                "Jane": 0
+            });
 
             makeTransactions(book);
 
@@ -113,8 +176,32 @@ describe("SwedishHBEF", function() {
             expect(balance.from).to.equal(undefined);
             expect(balance.to).to.equal(undefined);
             expect(balance.assets).to.equal(0);
-            expect(balance.debts).to.equal(0);
+            expect(balance.debts).to.equal(-1492.2);
+            expect(balance.ownCapital).to.equal(1492.2);
             expect(balance.valid).to.equal(true);
+            expect(balance.ownCapitalShare).to.eql({
+                "John": -2718.4,
+                "Jane": 4210.6
+            });
+
+            book.createVerification("2012-10-04", "Sold product").debit(1930, 7500).credit(2610, 1500).credit(3000, 6000);
+
+            balance = book.balance();
+            expect(balance).to.be.an("object");
+            expect(balance.from).to.equal(undefined);
+            expect(balance.to).to.equal(undefined);
+            expect(balance.assets).to.equal(7500);
+            expect(balance.debts).to.equal(7.8);
+            expect(balance.ownCapital).to.equal(7492.20);
+            expect(balance.valid).to.equal(true);
+            expect(balance.ownCapitalShare).to.eql({
+                "John": 281.6,
+                "Jane": 7210.6
+            });
+        });
+
+        it("should calculate the own capital by owner share correctly", function() {
+
         });
     });
 });
