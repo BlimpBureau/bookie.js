12501f7156b1266f7d0d901adc7193c17114c971
Lucas Wiener <lucas@wiener.se>
2014-12-29 15:26:24 +0100

    Renamed internal dateModule instance to dateUtils

--- src/date/date-utils.js
+++ src/date/date-utils.js
@@ -5,7 +5,7 @@ var _ = require("lodash");
 /**
  * @exports bookie/date
  */
-var dateModule = module.exports = {};
+var dateUtils = module.exports = {};
 
 /**
  * Parses the input into a date. The date will always be set to the time 00:00:00 GMT+0. Note that this discards the local timezone, so GMT+1 will still be 00:00:00.
@@ -14,7 +14,7 @@ var dateModule = module.exports = {};
  * @param {date|string} date The date to parse to a timeless `date` object. If date, the time part will simply be set to the specification. If string, it will be parsed to the given date.
  * @returns {?date} The parsed date. If invalid input is given the function will return null.
  */
-dateModule.parse = function(date) {
+dateUtils.parse = function(date) {
     //Make sure the date has the time 00:00:00 GMT.
     //Note that if there is local timezone offset the time will be the same as the time zone offset. So GMT+1 will yield the time 01:00:00.
     function createDate(year, month, day) {
@@ -56,8 +56,8 @@ dateModule.parse = function(date) {
  * @param {string|date} date The date to serialize. If string, it will be parsed as a date before serializing.
  * @returns {?string} The serialized string representation of the date. Null if invalid input.
  */
-dateModule.stringify = function(date) {
-    date = dateModule.parse(date);
+dateUtils.stringify = function(date) {
+    date = dateUtils.parse(date);
 
     if(!date) {
         return null;
@@ -77,9 +77,9 @@ dateModule.stringify = function(date) {
  * @param {string|date} date2 The second date to check for equality with the first date. If string, it will be parsed to a date.
  * @returns {boolean} True if the given dates are equal. False if they are not equal or if any of the dates are invalid.
  */
-dateModule.isEqual = function(date1, date2) {
-    date1 = dateModule.parse(date1);
-    date2 = dateModule.parse(date2);
+dateUtils.isEqual = function(date1, date2) {
+    date1 = dateUtils.parse(date1);
+    date2 = dateUtils.parse(date2);
 
     if(!date1 || !date2) {
         return false;
@@ -96,8 +96,8 @@ dateModule.isEqual = function(date1, date2) {
  * @param {?(string|date)} to The higher inclusive bound for the date range. No higher bound if null.
  * @returns {boolean} True if the date is inside the given date range. False if not or if date is invalid.
  */
-dateModule.isInsideDates = function(date, from, to) {
-    date = dateModule.parse(date);
+dateUtils.isInsideDates = function(date, from, to) {
+    date = dateUtils.parse(date);
 
     if(!date) {
         return false;
@@ -108,7 +108,7 @@ dateModule.isInsideDates = function(date, from, to) {
             return value;
         }
 
-        return value ? dateModule.parse(value) : value;
+        return value ? dateUtils.parse(value) : value;
     }
 
     if(!_.isDate(date)) {
